44fa29ee2a867a78676b95dcd25c9985b00a2141
Naman Kedia <naman_kedia@dell.com>
2020-09-25 23:20:20 +0530

    final first code

--- src/Codeforces/Cube_Sorting.java
+++ src/Codeforces/Cube_Sorting.java
@@ -52,20 +52,25 @@ import java.util.Scanner;
 public class Cube_Sorting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t>0){
-            int n=sc.nextInt();
-            int a[]=new int[n];
-            for(int i=0;i<n;i++){
-                a[i]=sc.nextInt();
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
             }
-            for(int i=0;i<n-1;i++) {
-                if (a[i] <= a[i + 1])
+            int flag=0;
+            t = t - 1;
+            for (int i = 0; i < n - 1; i++) {
+                if (a[i] <= a[i + 1]) {
                     System.out.println("YES");
-                return;
+                    flag=1;
+                    i=n-1;
+                }
             }
+            if(flag==0)
             System.out.println("NO");
-            t=t-1;
+
         }
     }
 }
