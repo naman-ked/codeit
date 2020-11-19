4b41b4d737569fd01b36bc25fd615d7863e674fc
Naman kedia <60963355+ked27@users.noreply.github.com>
2020-11-19 10:42:57 +0530

    Update ADADISH.java

--- src/Codechef/ADADISH.java
+++ src/Codechef/ADADISH.java
@@ -33,43 +33,38 @@ Example Output
 3
 7
  */
-package Codechef;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
-
-public class ADADISH {
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int t= sc.nextInt();
-        while(t-->0)
-        {
-            int n= sc.nextInt();
-            int a[]= new int[n];
-            int b[]= new int[n];
-            for(int i=0; i<n; i++)
-                b[i]= sc.nextInt();
-
-            int j=1;
-            for(int i=n-1; i>=0; i--)
-            {
-                if(b[i]==(i+1))
-                {
-                    a[i]=j;
-                    j++;
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	 {
+	    Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            Arrays.sort(a);
+            int c1=0,c2=0,i=n-1,time;
+            while(i>=0){
+                while(c1<=c2 && i>=0){
+                    c1+=a[i];
+                    i=i-1;
                 }
-                else
-                {
-                    a[i]=a[b[i]-1];
+                while(c2<c1 && i>=0){
+                    c2+=a[i];
+                    i=i-1;
                 }
             }
-            for(int i=0; i<n; i++)
-            {
-                System.out.print(a[i]+" ");
-            }
-            System.out.println();
+            time=Math.max(c2,c1);
+            System.out.println(time);
+            t=t-1;
         }
     }
 }
