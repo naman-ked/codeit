be6059d7d32b2d8f8bbe60f3ae0b253510a82efe
Naman kedia <60963355+ked27@users.noreply.github.com>
2021-04-20 15:41:12 +0530

    Update Prog_6.py

--- src/Practice/Array/Prog_6.py
+++ src/Practice/Array/Prog_6.py
@@ -1,13 +1,1 @@
-#Find the Duplicate Number
-#Solution 1:
-#O(nlogn) Time | O(1) Space 
-# class Solution:
-#     def findDuplicate(a):
-#         a.sort()
-#         n=len(a)
-#         for i in range(0,n):
-#             if(a[i]==a[i+1] and i<n):
-#                 print(a[i])
-#     a=[2,2,2,2,2]
-#     findDuplicate(a)
-#Solution 2:
+
