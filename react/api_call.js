fb1cc01c2ddc5173bdd6eabe14f1740ad8f24bab
Naman kedia <60963355+ked27@users.noreply.github.com>
2022-10-13 11:14:03 +0530

    Update api_call.js

--- react/api_call.js
+++ react/api_call.js
@@ -28,3 +28,30 @@ render(){
 export default App;
 
 //Functional Component
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [joke, setJoke] = useState({});
+
+  useEffect(() => {
+    console.log("componentDidMount");
+    async function fetchData() {
+    const joke=await fetch("https://reqres.in/api/users?page=2").then(res=>res.json())
+    console.log(joke.data[0])
+    setJoke(joke.data[0]);
+  }
+  fetchData();
+  },[]);
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+
+      <h3>Fetch data from API</h3>
+      <h4>{joke.id}</h4>
+      <h4>{joke.email}</h4>
+    </div>
+  );
+}
