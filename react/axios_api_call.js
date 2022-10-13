a66106013de3024419404ec771e64b6333969343
Naman kedia <60963355+ked27@users.noreply.github.com>
2022-10-13 11:20:20 +0530

    Update axios_api_call.js

--- react/axios_api_call.js
+++ react/axios_api_call.js
@@ -1,4 +1,32 @@
 //Class component
+import axios from "axios";
+import {Component} from "react";
+import "./styles.css";
+class App extends Component {
+  state={
+    joke:""
+  }
+  async componentDidMount(){
+    console.log("componentDidMount");
+    let url="https://official-joke-api.appspot.com/random_joke"
+    const joke=await axios.get(url)
+    this.setState({joke:joke.data})
+  }
+
+render(){
+  console.log("render");
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <h3>Fetch data from API</h3>
+      <h4>{this.state.joke.setup}</h4>
+      <h4>{this.state.joke.punchline}</h4>    
+    </div>
+  );
+}
+}
+export default App;
 
 //Functional component
 import axios from "axios";
