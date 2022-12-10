89fe03ac89b6bbd5be5fad4ad035891ec4e18b63
Naman kedia <60963355+ked27@users.noreply.github.com>
2022-12-10 22:48:48 +0530

    Update Day_5-Buld_The_Smallest.cpp

--- potd/Day_5-Buld_The_Smallest.cpp
+++ potd/Day_5-Buld_The_Smallest.cpp
@@ -11,3 +11,31 @@ possible after removing '4' and '7'.
 
 
 */
+
+string buildLowestNumber(string num, int k)
+{
+    stack<char> ch;
+    for(char c:num){
+        while(ch.size()>0 and ch.top()>c and k>0){
+            ch.pop();
+            k--;
+        }
+        ch.push(c);
+    }
+    while(k>0 and ch.size()>0){
+        ch.pop();
+        k--;
+    }
+    string s="";
+    while(ch.size()>0){
+        s=ch.top()+s;
+        ch.pop();
+    }
+    
+    int i=0;
+    while(s[i]=='0' and i<s.size())i++;
+    
+    s=s.substr(i);
+    if(s=="")s="0";
+    return s;
+}
