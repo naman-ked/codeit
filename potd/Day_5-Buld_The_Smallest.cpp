66846e6d3ef1441935c91711d6e9c3ac3eedfe37
Naman kedia <60963355+ked27@users.noreply.github.com>
2022-12-10 22:48:15 +0530

    Create Day_5-Buld_The_Smallest.cpp

---
+++ potd/Day_5-Buld_The_Smallest.cpp
@@ -0,0 +1,13 @@
+/* 
+Given a number k and string num of digits (0 to 9) denoting a positive integer. Find a string denoting the lowest integer number possible by removing k digits from num, without changing their order.
+Note: num will not contain any leading zero.
+
+Input:
+k = 2
+num = "143729"
+Output: "1329"
+Explanation: 1329 is the minimum number
+possible after removing '4' and '7'.
+
+
+*/
