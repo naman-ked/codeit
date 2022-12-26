0f3f792ae1072425dd0d22961d70ac958f9c09fd
Naman kedia <60963355+ked27@users.noreply.github.com>
2022-12-26 14:09:18 +0530

    Update Day_16-Akku_Binary_Numbers.cpp

--- potd/Day_16-Akku_Binary_Numbers.cpp
+++ potd/Day_16-Akku_Binary_Numbers.cpp
@@ -41,4 +41,27 @@ public:
     }
 };
 */
-
+class Solution{
+public:
+vector<long long>pre;
+     void precompute()
+    {
+       for(int i=0;i<63;i++){
+           for(int j=i+1;j<63;j++){
+               for(int k=j+1;k<63;k++){
+                   long long tmp=pow(2,i)+pow(2,j)+pow(2,k);
+                   pre.push_back(tmp);
+               }
+           }
+       }
+       sort(pre.begin(),pre.end());
+    }
+    
+    long long solve(long long l, long long r){
+        // Code Here
+            int hi=upper_bound(pre.begin(),pre.end(),r)-pre.begin();
+            int lo=lower_bound(pre.begin(),pre.end(),l)-pre.begin();
+            return hi-lo;
+    }
+    
+};
