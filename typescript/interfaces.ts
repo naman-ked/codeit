67c3f63fe4e2e91b51e692ad264a349e6318f679
Naman kedia <60963355+ked27@users.noreply.github.com>
2023-01-16 22:10:55 +0530

    Update interfaces.ts

--- typescript/interfaces.ts
+++ typescript/interfaces.ts
@@ -1,15 +1,32 @@
-interface Person{
-    name:string;
-    age:number;
-
+type Persontype = {
+    readonly name:string;
     greet(phrase: string): void;
 }
+
+interface Person{
+    readonly name:string;
+    greet(phrase: string): void;
+}
+
+class Person2 implements Person{
+   name:string;
+   age=24;
+   role="developer";
+   constructor(n: string){
+    this.name=n;
+   }
+   greet(phrase:string){
+    console.log(this.name+" "+phrase+" "+this.role);
+   }
+}
+let user= new Person2("naman");
+console.log(user);
+
 
 let user1:Person;
 
 user1={
     name:'Naman',
-    age:24,
     greet(phrase: string){
         console.log(phrase+" "+this.name);
     }
