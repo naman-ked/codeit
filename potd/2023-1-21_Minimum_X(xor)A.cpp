cf4cf644ce909447bac403e48a053236ffd554d1
Naman kedia <60963355+ked27@users.noreply.github.com>
2023-01-26 15:34:58 +0530

    Arrays Bit Magic

--- potd/2023-1-21_Minimum_X(xor)A.cpp
+++ potd/2023-1-21_Minimum_X(xor)A.cpp
@@ -3,7 +3,6 @@
  and the count of set bit in X is equal to the count of set bits in B.
 
 Example 1:
-
 Input: 
 A = 3, B = 5
 Output: 3
@@ -11,9 +10,7 @@ Explanation:
 Binary(A) = Binary(3) = 011
 Binary(B) = Binary(5) = 101
 The XOR will be minimum when x = 3
-i.e. (3 XOR 3) = 0 and the number
-of set bits in 3 is equal
-to the number of set bits in 5.
+i.e. (3 XOR 3) = 0 and the number of set bits in 3 is equal to the number of set bits in 5.
  */
  
  class Solution {
