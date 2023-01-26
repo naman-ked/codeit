392f56b8499bd4688ba78c27db2c4cb17cd9237c
Naman kedia <60963355+ked27@users.noreply.github.com>
2023-01-26 16:25:18 +0530

    Strings implementation

--- potd/2022-12-15_Balanced_string.cpp
+++ potd/2022-12-15_Balanced_string.cpp
@@ -1,21 +1,13 @@
 /*
 Given an integer N.Create a string using only lowercase characters from a to z that follows the given rules.
-
 When N is even:
-
 Use N/2 characters from the beginning of a-z and N/2 characters from the ending of a-z.
-
 When N is greater than 26,continue repeating the instructions until length of string becomes N.
 
 When N is odd:
-
 Case 1: If the sum of digits of N is even, Select (N+1)/2 characters from the beginning of a-z and (N-1)/2 characters from the ending of a-z.
-
 Case 2: If the sum of digits of N is odd, Select (N-1)/2 characters from the beginning of a-z and (N+1)/2 characters from the ending of a-z.
-
 When N is greater than 26,continue repeating the instructions until length of string becomes N.
-
- 
 
 Example 1:
 
@@ -24,21 +16,15 @@ N=21
 Output: 
 abcdefghijpqrstuvwxyz
 Explanation:
-Since 21 is odd and sum of digits
-of 21 is also odd,we take (21-1)/2=10 
-characters from the beginning and 
-(21+1)/2=11 characters from the 
-end of a-z.
-Example 2:
+Since 21 is odd and sum of digits of 21 is also odd,we take (21-1)/2=10  characters from the beginning and  (21+1)/2=11 characters from the end of a-z.
 
+Example 2:
 Input:
 N=28 
 Output: 
 abcdefghijklmnopqrstuvwxyzaz
 Explanation: 
-Since 28>26, we keep repeating 
-the process until length of string becomes 
-28.
+Since 28>26, we keep repeating the process until length of string becomes 28.
 */
 
 
