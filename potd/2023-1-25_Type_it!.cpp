19f46d9da5326d0902c9f538295989f8cfe5ade2
Naman kedia <60963355+ked27@users.noreply.github.com>
2023-01-26 15:24:37 +0530

    Strings

--- potd/2023-1-25_Type_it!.cpp
+++ potd/2023-1-25_Type_it!.cpp
@@ -2,13 +2,11 @@
 Geek is extremely punctual but today even he is not feeling like doing his homework assignment.
 He must start doing it immediately in order to meet the deadline. For the assignment, Geek needs to type a string s.
 To reduce his workload, he has decided to perform one of the following two operations till he gets the string.
-
 Append a character at the end of the string.
 Append the string formed thus far to the end of the string. (This can not be done more than once.)
 Help Geek find the minimum operations required to type the given string.
 
 Example 1:
-
 Input:
 s = abcabca
 Output: 5
