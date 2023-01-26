aebda24344be87e57c1e96e389d945460a7f7a25
Naman kedia <60963355+ked27@users.noreply.github.com>
2023-01-26 16:19:11 +0530

    Strings Stack Mathematical

--- potd/2022-12-10-Build_The_Smallest.cpp
+++ potd/2022-12-10-Build_The_Smallest.cpp
@@ -1,5 +1,6 @@
 /* 
-Given a number k and string num of digits (0 to 9) denoting a positive integer. Find a string denoting the lowest integer number possible by removing k digits from num, without changing their order.
+Given a number k and string num of digits (0 to 9) denoting a positive integer. 
+Find a string denoting the lowest integer number possible by removing k digits from num, without changing their order.
 Note: num will not contain any leading zero.
 
 Input:
@@ -8,8 +9,6 @@ num = "143729"
 Output: "1329"
 Explanation: 1329 is the minimum number
 possible after removing '4' and '7'.
-
-
 */
 
 string buildLowestNumber(string num, int k)
