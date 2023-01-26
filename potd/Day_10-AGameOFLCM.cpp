536999de68de2c60438eba53cb4a90a117b9f188
Naman kedia <60963355+ked27@users.noreply.github.com>
2023-01-26 16:29:39 +0530

    Misc Mathematical

--- potd/Day_10-AGameOFLCM.cpp
+++ potd/Day_10-AGameOFLCM.cpp
@@ -3,16 +3,12 @@ Given an integer N. Find maximum LCM (Least Common Multiple) that can be obtaine
 Note: Duplicate numbers can be used.
 
 Example 1:
-
 Input:
 N = 4
 Output: 12
 Explanation:
-The four numbers can be [4,4,3,2] or
-[4,4,4,3], etc. It can be shown that 12 is
-the maximum LCM of four numbers that can
-be obtained from numbers less than or equal 
-to 4.
+The four numbers can be [4,4,3,2] or [4,4,4,3], etc. It can be shown that 12 is the maximum LCM of four numbers 
+that can be obtained from numbers less than or equal to 4.
 */
 class Solution {
   public:
