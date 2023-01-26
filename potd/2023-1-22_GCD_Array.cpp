11eb4797637adab35a31c944d9f8790656fb0862
Naman kedia <60963355+ked27@users.noreply.github.com>
2023-01-26 16:50:06 +0530

    Arrays Mathematical

--- potd/2023-1-22_GCD_Array.cpp
+++ potd/2023-1-22_GCD_Array.cpp
@@ -3,9 +3,7 @@ You are given an array, arr of length N, and also a single integer K .
 Your task is to split the array arr into K non-overlapping, non-empty subarrays. 
 For each of the subarrays, you calculate the sum of the elements in it. Let us denote these sums as S1, S2, S3, ..., SK. 
 Where Si denotes the sum of the elements in the ith subarray from left.
-
 Let G = GCD( S1, S2 ,S3 , ..., SK).
-
 Find the maximum value of G that can be obtained. 
 The array may contain duplicate elements.
 
@@ -16,13 +14,9 @@ K = 4
 arr[] = {6, 7, 5, 27, 3}
 Output: 3
 Explanation: 
-Since K = 4, you have to split the array into 4 subarrays.
-For optimal splitting, split the array into
-4 subarrays as follows: [[6], [7, 5], [27], [3]]
-Therefore, S1 = 6, S2 = 7 + 5 = 12, S3 = 27, S4 = 3
-Hence, G = GCD(S1, S2, S3, S4) = GCD(6, 12, 27, 3) = 3
-It can be shown that 3 is the maximum value of G that can be obtained.
-Thus, the answer is 3.
+Since K = 4, you have to split the array into 4 subarrays.For optimal splitting, split the array into 4 subarrays as follows: [[6], [7, 5], [27], [3]]
+Therefore, S1 = 6, S2 = 7 + 5 = 12, S3 = 27, S4 = 3 .Hence, G = GCD(S1, S2, S3, S4) = GCD(6, 12, 27, 3) = 3
+It can be shown that 3 is the maximum value of G that can be obtained. Thus, the answer is 3.
 */
 
 class Solution {
