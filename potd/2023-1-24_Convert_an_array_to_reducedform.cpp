014fdb8331daab2675eabc51c36f4227b7170598
Naman kedia <60963355+ked27@users.noreply.github.com>
2023-01-26 15:25:50 +0530

    Arrays Sorting

--- potd/2023-1-24_Convert_an_array_to_reducedform.cpp
+++ potd/2023-1-24_Convert_an_array_to_reducedform.cpp
@@ -4,16 +4,13 @@ The order of elements is same, i.e., 0 is placed in place of smallest element, 1
 Note: You don't have to return anything. You just have to change the the given array.
 
 Example 1:
-
 Input:
 N = 3
 Arr[] = {10, 40, 20}
 Output: 0 2 1
-Explanation: 10 is the least element so it
-is replaced by 0. 40 is the largest
-element so it is replaced by 3-1 = 2. And
-20 is the 2nd smallest element so it is
-replaced by 1.
+Explanation: 10 is the least element so it is replaced by 0. 
+40 is the largest element so it is replaced by 3-1 = 2. 
+And 20 is the 2nd smallest element so it is replaced by 1.
 */
 
 class Solution{
