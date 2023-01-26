fecf161454636f6410b795465be1ed9638d64cc9
Naman kedia <60963355+ked27@users.noreply.github.com>
2023-01-26 15:56:57 +0530

    Arrays

--- potd/2022-11-22_Magic_Triplets.java
+++ potd/2022-11-22_Magic_Triplets.java
@@ -1,9 +1,7 @@
 
 /* 
-
 Given an array of size n, a triplet (a[i], a[j], a[k]) is called a Magic Triplet if a[i] < a[j] < a[k] and i < j < k.  
 Count the number of magic triplets in a given array.
- 
 
 Example 1:
 
@@ -18,12 +16,10 @@ Output: 4
 Explanation: Fours magic triplets are 
 (1, 2, 3), (1, 2, 4), (1, 3, 4) and 
 (2, 3, 4).
- 
 
 Your Task:
 You don't need to read or print anything. Your task is to complete the function countTriplets() which takes the array nums[] as input parameter and 
 returns the number of magic triplets in the array.
-
  
 Expected Time Complexity: O(N2) 
 Expected Space Complexity: O(1)
