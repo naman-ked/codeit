434cbebc3aa8cb180aaa0974f60b024feccc7cd0
Naman kedia <60963355+naman-ked@users.noreply.github.com>
2023-01-29 07:21:02 +0530

    Create 2023-01-29_Min_operations.cpp

---
+++ potd/2023-01-29_Min_operations.cpp
@@ -0,0 +1,23 @@
+/*
+Given two numbers a and b. In one operation you can pick any non negative integer x and either of a or b. 
+Now if you picked a then replace a with a&x else if you picked b then replace b with b&x.
+Return the minimum number of operation required to make a and b equal.
+Note: Here & represents bitwise AND operation.
+
+Example 1:
+Input:
+a = 5, b = 12
+Output: 2
+Explanantion:In first operation replace a = a&4 = 4after that replace b = b&6 = 4Hence both are same after applying twooperations.
+*/
+
+class Solution {
+  public:
+    int solve(int a, int b) {
+        // code here
+        if(a==b) return 0;
+        int c=a&b;
+        if(c==a || c==b) return 1;
+        else return 2;
+    }
+};
