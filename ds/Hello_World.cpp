42f2e1d45eca6000b53a3286fa17c5fe65fbc4a5
Naman kedia <60963355+naman-ked@users.noreply.github.com>
2023-02-22 19:52:51 +0530

    Create Hello_World.cpp

---
+++ ds/Hello_World.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	cout<<"Hello World!" <<endl;
+    
+    int physics=66;
+    int math=90;
+    float avg = (physics + math)/2.0;
+
+    cout<<"Average is "<<avg;
+	return 0;
+}
