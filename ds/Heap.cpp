cd9bbde0e843c1d31ca55a1fec96dc606ce28721
Naman kedia <60963355+naman-ked@users.noreply.github.com>
2023-02-22 19:49:13 +0530

    Create Heap.cpp

---
+++ ds/Heap.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class compare{
+public:
+  bool operator()(int a , int b){
+     return a>b;
+       }
+};
+int main(){
+	int arr[]={10,15,20,13,6,90};
+	int n=sizeof(arr)/sizeof(int);
+    //priority_queue<int> heap;  --max heap
+	priority_queue<int,vector<int>,compare> heap;
+	for(int x:arr){
+		heap.push(x);
+	}
+	while(!heap.empty()){
+		cout<<heap.top() <<endl;
+		heap.pop();
+	}
+	return 0;
+}
