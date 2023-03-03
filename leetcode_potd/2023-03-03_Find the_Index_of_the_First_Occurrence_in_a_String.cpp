8d2057cdbcb88653c528e457066f79f5004bb048
Naman kedia <60963355+naman-ked@users.noreply.github.com>
2023-03-03 21:57:13 +0530

    Two Pointers String String Matching

---
+++ leetcode_potd/2023-03-03_Find the_Index_of_the_First_Occurrence_in_a_String.cpp
@@ -0,0 +1,40 @@
+/*
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.The first occurrence is at index 0, so we return 0.
+
+Example 2:
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+*/
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int hLen = haystack.length();
+        int nLen = needle.length();
+        int nIndex = 0;
+        for(int i=0; i<hLen; i++){
+            // as long as the characters are equal, increment needleIndex
+            if(haystack[i]==needle[nIndex]){
+                nIndex++;
+            }
+            else{
+                // start from the next index of previous start index
+                i=i-nIndex;
+                // needle should start from index 0
+                nIndex=0;
+            }
+            // check if needleIndex reached needle length
+            if(nIndex==nLen){
+                // return the first index
+                return i-nLen+1;
+            }
+        }
+        return -1;
+    }
+};
