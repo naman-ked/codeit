1c94a0153ca0deab6576340c463cbb4f699e50ef
Naman kedia <60963355+naman-ked@users.noreply.github.com>
2023-03-13 17:38:11 +0530

    Mathematical

---
+++ potd/2023-03-13_Maximum_Possible_Value.cpp
@@ -0,0 +1,45 @@
+/*
+Given two arrays A[] and B[] of same length N. 
+There are N types of sticks of lengths specified. 
+Each stick of length A[i] is present in B[i] units (i=1 to N). You have to construct the squares and rectangles using these sticks.
+Each unit of a stick can be used as length or breadth of a rectangle or as a side of square. A single unit of a stick can be used only once.
+Let S be the sum of lengths of all sticks that are used in constructing squares and rectangles. The task is to calulate the maximum possible value of S.
+
+Example 1:
+Input:
+N = 4
+A[] = {3,4,6,5}
+B[] = {2,3,1,6}
+Output: 38
+Explanation: 
+There are 2 sticks of length 3.
+There are 3 sticks of length 4.
+There is a 1 stick of length 6.
+There are 6 sticks of length 5.
+One square can be made using 4 sticks of4th kind (5*4=20)
+A rectangle can be made using 2 sticks of 4th kind and 2 sticks of 2nd kind (5*2+4*2=18)S = 20 + 18 = 38
+*/
+
+class Solution {
+  public:
+    long long maxPossibleValue(int N,vector<int> A, vector<int> B) {
+        // code here
+        long long ans =0;
+        int totalsticks=0;
+        int mn=INT_MAX;
+        for(int i=0;i<N;i++){
+            int len=A[i];
+            int units=B[i];
+            if(units%2!=0){
+                units--;
+            }
+            if(units>=2) mn=min(mn,len);
+            ans+=(len*units);
+            totalsticks+=units;
+        }
+        if(totalsticks%4!=0){
+            ans-=(2*mn);
+        }
+        return ans;
+    }
+};
