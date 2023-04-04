d658231deefed679111dff28ff8ce47c35becb5f
Naman kedia <60963355+naman-ked@users.noreply.github.com>
2023-04-04 15:21:32 +0530

    Update Reverse_Words_of_String.js

--- js/Reverse_Words_of_String.js
+++ js/Reverse_Words_of_String.js
@@ -3,3 +3,14 @@ Input : "This is Javascript Code"
 
 */
 
+let s="This is a Javasript code";
+
+const arr= s.split(' ');
+let reversedArr= [];
+
+console.log(arr); //[ 'This', 'is', 'a', 'Javasript', 'code' ]
+
+reversedArr= arr.map((item)=>item.split('').reverse().join('')).join(' ');
+
+console.log(reversedArr)  //sihT si a tpirsavaJ edoc
+
