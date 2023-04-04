0f18381fa52252e2ee0d53c1042df90d1af20fef
Naman kedia <60963355+naman-ked@users.noreply.github.com>
2023-04-04 15:55:24 +0530

    Update Max_character_in_a_string.js

--- js/Max_character_in_a_string.js
+++ js/Max_character_in_a_string.js
@@ -7,3 +7,20 @@ O/P - c
 I/P - apple1231111
 O/P - 1
 */
+function maxCharacter(str){
+  const charMap = {};
+  let max = 0, maxChar = "";
+
+  for(let char of str){
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for(let char in charMap){
+    if(charMap[char] > max){
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  
+  return maxChar;
+}
