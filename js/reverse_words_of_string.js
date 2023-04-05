9141764f61da676368342f227cd4215b46184f8c
Naman kedia <60963355+naman-ked@users.noreply.github.com>
2023-04-05 05:46:40 +0530

    Update reverse_words_of_string.js

--- js/reverse_words_of_string.js
+++ js/reverse_words_of_string.js
@@ -10,3 +10,6 @@ console.log(arr); //[ 'This', 'is', 'a', 'Javasript', 'code' ]
 reversedArr= arr.map((item)=>item.split('').reverse().join('')).join(' ');
 
 console.log(reversedArr)  //sihT si a tpirsavaJ edoc
+
+// Yes, it is possible in Js to create a reverse() method that can reverse any string 
+// and it can be done by adding your custom reverse logic to String.prototype
