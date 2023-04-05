b18378037ffc16c72784668d5ea064bca2200cf3
Naman kedia <60963355+naman-ked@users.noreply.github.com>
2023-04-05 10:59:11 +0530

    Update polyfill_promise.js

--- js/polyfill_promise.js
+++ js/polyfill_promise.js
@@ -1,1 +1,44 @@
+function Pro(executor){
+    let onResolve, onReject;
+    let isFullfilled =false,isCalled=false,value;
+    function resolve(val){
+        isFullfilled=true;
+        value=val;
+        if(typeof onResolve === 'function'){
+            onResolve(value);
+            isCalled=true;
+        }
+        
+    }
+    
+    function reject(value){
+        
+        onReject(value);    
+    }
+    
+    this.then = function (callback) {
+        onResolve = callback;
+        if(isFullfilled && !isCalled){
+            called=true;
+            onResolve(value);
+        }
+        return this;
+    }
+    
+    this.catch = function(callback) {
+      return this;  
+    };
+    
+    executor(resolve,reject);
+}
 
+
+const examplePromise = new Pro((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(2);
+    },1000);
+});
+
+examplePromise.then((res)=>{
+    console.log(res);
+}).catch((err)=>{console,log(err)});
