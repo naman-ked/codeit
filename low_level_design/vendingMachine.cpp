afcbbaf95a52f14319802f4919d8942688cbbcd4
Naman kedia <60963355+naman-ked@users.noreply.github.com>
2024-05-19 16:01:18 +0530

    Update vendingMachine.cpp

--- low_level_design/vendingMachine.cpp
+++ low_level_design/vendingMachine.cpp
@@ -1,1 +1,9 @@
+Requirements: 
+1. Ability to select an item
+2. Ability to pay for a item (cash / credit card)
+3. Dispense the selected item
+4. Notify the owner (serving agent) about inventory status (running low on change or items)
+5. Payment gateway system for credit card
+
+
 
