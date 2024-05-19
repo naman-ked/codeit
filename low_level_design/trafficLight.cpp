38ae634c0cefe36a1906274fec6744abbb19dd00
Naman kedia <60963355+naman-ked@users.noreply.github.com>
2024-05-19 13:59:07 +0530

    Update trafficLight.cpp

--- low_level_design/trafficLight.cpp
+++ low_level_design/trafficLight.cpp
@@ -1,63 +1,62 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
+using namespace std;
 
-// State Interface
-class TrafficLightState {
-public:
-	virtual void handle() = 0;
+//Interface which contains method for all states
+class TrafficLightState{
+    public:
+    virtual void handle()=0;
 };
 
-// Concrete States
-class RedState : public TrafficLightState {
-public:
-	void handle() override
-	{
-		std::cout << "Traffic Light is Red\n";
-	}
+class GreenState : public TrafficLightState{
+    public:
+    void handle() override
+    {
+        cout<< "Traffic light 🤢"<<endl;
+    }
 };
 
-class YellowState : public TrafficLightState {
-public:
-	void handle() override
-	{
-		std::cout << "Traffic Light is Yellow\n";
-	}
+class RedState : public TrafficLightState{
+    public:
+    void handle() override
+    {
+        cout<< "Traffic light 😡"<<endl;
+    }
 };
 
-class GreenState : public TrafficLightState {
-public:
-	void handle() override
-	{
-		std::cout << "Traffic Light is Green\n";
-	}
+class YellowState : public TrafficLightState{
+    public:
+    void handle() override
+    {
+        cout<< "Traffic light 😊"<<endl;
+    }
 };
 
-// Context
-class TrafficLight {
-private:
-	TrafficLightState* state;
-
-public:
-	TrafficLight()
-		: state(new RedState())
-	{
-	}
-
-	void setState(TrafficLightState* newState)
-	{
-		delete state;
-		state = newState;
-	}
-
-	void change() { state->handle(); }
+class TrafficLight{
+    private:
+    TrafficLightState* state;
+    public:
+    TrafficLight() : state(new RedState()) {}
+    void setState(TrafficLightState* newState)
+    {
+        delete state;
+        state = newState;
+    }
+    
+    void change(){
+        state->handle();
+    }
 };
 
 int main()
 {
-	TrafficLight trafficLight;
+	TrafficLight t;
 
-	trafficLight.change(); // Initial state: Red
-	trafficLight.setState(new GreenState());
-	trafficLight.change(); // State changed to Green
+	t.change(); // Initial state: Red
+	t.setState(new YellowState());
+	t.change();
+	t.setState(new GreenState());
+	t.change(); // State changed to Green
 
 	return 0;
 }
