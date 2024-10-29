956172bc4698153f3e787a7f4db26262123f085b
Naman kedia <60963355+naman-ked@users.noreply.github.com>
2024-10-30 04:06:57 +0530

    Create Two Sum.cpp

---
+++ Arrays/Two Sum.cpp
@@ -0,0 +1,44 @@
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+        int n = arr.size();
+        unordered_set<int> s;
+        s.insert(arr[0]);
+        for(int i=1;i<n;i++){
+            int diff = target-arr[i];
+            if(s.find(diff) != s.end()){
+                return true;
+            }
+            s.insert(arr[i]);
+        }
+        return false;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--) {
+        int x;
+        cin >> x;
+        cin.ignore(); // To discard any leftover newline characters
+
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        auto ans = ob.twoSum(arr, x);
+        cout << (ans ? "true" : "false") << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
